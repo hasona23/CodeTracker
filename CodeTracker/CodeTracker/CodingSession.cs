ece63586eaadbb65dfff950eba67583c6bff99e0
hasona23 <130175104+hasona23@users.noreply.github.com.>
2024-10-02 15:56:48 +0300

    First commit

---
+++ CodeTracker/CodeTracker/CodingSession.cs
@@ -0,0 +1,6 @@
+namespace CodeTracker;
+
+public class CodingSession
+{
+    
+}
---
+++ CodeTracker/CodeTracker/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeTracker;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
